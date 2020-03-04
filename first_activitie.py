8b038807eeef3866e356470dc865a36ef3ed2ac4
Alexjr2404 <61765664+Alexjr2404@users.noreply.github.com>
2020-03-03 19:42:58 -0600

    Add files via upload
    .py

---
+++ first_activitie.py
@@ -0,0 +1,13 @@
+Name="Aleiandro"
+Last_name="Cervantes"
+Age="18 years"
+Number="7713965708"
+ID="Id 93111914"
+Gamertag="Alexjr2404"
+
+print('Name:{}'.format(Name))
+print('Last Name:{}'.format(Last_name))
+print('Age:{}'.format(Age))
+print('Number"{}'.format(Number))
+print('ID:{}'.format(ID))
+print('Gamertag:{}'.format(Gamertag))
